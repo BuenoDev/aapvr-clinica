9af2ca29c3695d5d55c6a274b69043d718bf5718
valmeidavr <valmeida@idea-planejamento.com>
2019-11-23 19:25:45 -0300

    Adicionado Logotipo AAPVR

--- quasar/src/layouts/MainLayout.vue
+++ quasar/src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+            <img src="/statics/logo.webp">
              <!-- <img src="https://static.wixstatic.com/media/769b46_80e632a9b8e64f80a2b12a0fce072515~mv2.png/v1/fill/w_84,h_78,al_c,q_80,usm_0.66_1.00_0.01/769b46_80e632a9b8e64f80a2b12a0fce072515~mv2.webp" > -->
           </q-avatar>
           AAPVR - Associação de Aposentados de Volta Redonda
--- quasar/src/pages/Index.vue
+++ quasar/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <img alt="Associação dos Aposentados | AAP-VR | Brasil" src="/statics/logo.webp">
   </q-page>
 </template>
 
