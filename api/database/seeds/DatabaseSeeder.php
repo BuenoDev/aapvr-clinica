91230988fdf4a68081a6195ec7d14667df32a12b
Gustavo Bueno <gbueno360@gmail.com>
2019-12-27 23:50:35 -0300

    Melhorias necessarias

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -17,25 +17,31 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(AssignPermissionsSeeder::class);
+
         $this->call(EspecialidadeSeeder::class);
+        $this->call(ConselhoTableSeeder::class);
         /**
          * Abaixo apenas seeds em modo de desenvolvimento
          */
         $this->call(UsersTableSeeder::class);
-        $this->call(GrupoProcedimentoTableSeeder::class);
         $this->call(TipoPrestadorTableSeeder::class);
 
-        $this->call(ConselhoTableSeeder::class);
         $this->call(ConvenioTableSeeder::class);
         $this->call(PrestadoresTableSeeder::class);
-        $this->call(EnderecoTableSeeder::class);
-        // $this->call(MedicoTableSeeder::class);
-        $this->call(PacienteTableSeeder::class);
-        $this->call(TelefoneTableSeeder::class);
-        $this->call(ProcedimentoTableSeeder::class);
+        /**
+         * Endereço e telefone seeder não precisam ser chamados de forma automatica,
+         * considerando que esses registros pertencem as entidades Prestaodor, Unidade e Paciente
+         * devendo ser ligadas as mesmas
+         */
         $this->call(UnidadeTableSeeder::class);
-        
-
-        
+        $this->call(TelefoneTableSeeder::class);
+        $this->call(PacienteTableSeeder::class);
+        $this->call(EnderecoTableSeeder::class);
+        /**
+         * o mesmo vale para as tabelas abaixo, onde o grupo necessita
+         * ter 1 ou mais procedimentos
+         */
+        $this->call(GrupoProcedimentoTableSeeder::class);
+        $this->call(ProcedimentoTableSeeder::class);
     }
 }
